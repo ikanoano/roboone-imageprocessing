87980b22820a0a7edb2d8f0208bdfb3119bf4701
ikanoano <ikanoano@users.noreply.github.com>
2019-06-16 18:05:45 +0900

    add depth mask, improve visualizer

--- sandbox/men_do_kote/men_do_kote.cpp
+++ sandbox/men_do_kote/men_do_kote.cpp
@@ -24,14 +24,16 @@ cv::Mat bgr2hsv(const cv::Mat& bgr) {
   return hsv;
 }
 men_do_kote_t get_men_do_kote(
-    const cv::Mat& input,
-    cv::Mat& men_visual,
-    cv::Mat& do_kote_visual
+    const cv::Mat& color_in,
+    const cv::Mat& depth_in,
+    const int near,
+    const int far,
+    cv::Mat& men_do_kote_visual
 ) {
   const double
     resize_scale = 0.2,
     resize_scale_inv = 1/resize_scale;
-  const cv::GScalar  // HSV threshold for red MEN
+  const cv::GScalar   // HSV threshold for MEN, DO, and KOTE
     //                              H    S    V
     red_thresh_low1   (cv::Scalar(252, 128,  64)),
     red_thresh_up1    (cv::Scalar(255, 255, 255)),
@@ -41,78 +43,86 @@ men_do_kote_t get_men_do_kote(
     blue_thresh_up    (cv::Scalar(175, 255, 255)),
     yellow_thresh_low (cv::Scalar( 28,  96,  64)),
     yellow_thresh_up  (cv::Scalar( 44, 255, 255));
+  const cv::GScalar   // Depth threshold to distinguish opponent from background
+    thresh_near       (near),
+    thresh_far        (far);
   const auto kernel5 = cv::Mat::ones(5, 5, CV_8U)*255;
-  //cv::GMat
-  //  in, h, s, v,
-  //  resize    = cv::gapi::resize(in, cv::Size(), resize_scale, resize_scale);
-  //std::tie(h,s,v) = cv::gapi::split3(resize);
-  //cv::GComputation separate_hsv(cv::GIn(in), cv::GOut(v, s, h));
   const cv::GMat
-    in,
-    resize    = cv::gapi::resize(in, cv::Size(), resize_scale, resize_scale),
-    red1      = cv::gapi::inRange(resize, red_thresh_low1, red_thresh_up1),
-    red2      = cv::gapi::inRange(resize, red_thresh_low2, red_thresh_up2),
+    cin, din,
+    // depth
+    dresize   = cv::gapi::resize(din, cv::Size(), resize_scale, resize_scale),
+    dvalid    = cv::gapi::inRange(dresize, thresh_near, thresh_far),
+    blur_d    = cv::gapi::gaussianBlur(dvalid, cv::Size(7, 7), 2, 2),
+    dmask     = cv::gapi::cmpGE(blur_d, cv::GScalar(32)),
+    // color
+    cresize   = cv::gapi::resize(cin, cv::Size(), resize_scale, resize_scale),
+    red1      = cv::gapi::inRange(cresize, red_thresh_low1, red_thresh_up1),
+    red2      = cv::gapi::inRange(cresize, red_thresh_low2, red_thresh_up2),
     red       = cv::gapi::bitwise_or(red1, red2),
-    blue      = cv::gapi::inRange(resize, blue_thresh_low , blue_thresh_up),
-    yellow    = cv::gapi::inRange(resize, yellow_thresh_low, yellow_thresh_up),
-    blu_ylw   = cv::gapi::bitwise_or(blue, yellow),
-    erode_by  = cv::gapi::erode3x3(blu_ylw),
-    dilate_r  = cv::gapi::dilate3x3(red, 3),
-    dilate_by = cv::gapi::dilate3x3(erode_by, 3),
-    blur_r    = cv::gapi::gaussianBlur(red /*dilate_r*/, cv::Size(11, 11), 2, 2),
-    blur_by   = cv::gapi::gaussianBlur(blu_ylw /*dilate_by*/, cv::Size(15, 15), 2, 2),
-    thresh_by = cv::gapi::threshold(blur_by, cv::GScalar(16), cv::GScalar(255), cv::THRESH_BINARY);
+    blue      = cv::gapi::inRange(cresize, blue_thresh_low , blue_thresh_up),
+    yellow    = cv::gapi::inRange(cresize, yellow_thresh_low, yellow_thresh_up),
+    masked_r  = cv::gapi::mask(red, dmask),
+    masked_b  = cv::gapi::mask(blue, dmask),
+    masked_y  = cv::gapi::mask(yellow, dmask),
+    blur_r    = cv::gapi::gaussianBlur(masked_r, cv::Size(11, 11), 2, 2),
+    blur_b    = cv::gapi::gaussianBlur(masked_b, cv::Size(15, 15), 2, 2),
+    blur_y    = cv::gapi::gaussianBlur(masked_y, cv::Size(15, 15), 2, 2),
+    bin_b     = cv::gapi::threshold(blur_b, cv::GScalar(16), cv::GScalar(255), cv::THRESH_BINARY),
+    bin_y     = cv::gapi::threshold(blur_y, cv::GScalar(16), cv::GScalar(255), cv::THRESH_BINARY),
+    merge     = cv::gapi::merge3(bin_b, bin_y, blur_r);
+  // hsv
+  //const cv::GMat h, s, v;
+  //std::tie(h,s,v) = cv::gapi::split3(cresize);
 
-  cv::GComputation color2blur(cv::GIn(in), cv::GOut(blue, blur_r, thresh_by));
+  assert(color_in.size() == depth_in.size());
 
-  cv::Mat men_im, do_kote_im, blue_im;
-  color2blur.apply(cv::gin(bgr2hsv(input)), cv::gout(blue_im, men_im, do_kote_im));
+  cv::GComputation color2mdk(cv::GIn(cin, din), cv::GOut(blur_r, bin_b, bin_y, merge));
+
+  cv::Mat men_im, do_im, kote_im;
+  color2mdk.apply(cv::gin(bgr2hsv(color_in), depth_in), cv::gout(men_im, do_im, kote_im, men_do_kote_visual));
 
   men_do_kote_t mdk = {{}, {}, {}};
 
   // detect MEN
-  const int dp=2, minDist=men_im.rows/8, canny_thresh_high=32, thresh_find=30, minRadius=10, maxRadius=30;
+  const int dp=2, minDist=men_im.rows/8, canny_thresh_high=32, thresh_find=30, minRadius=6, maxRadius=30;
   HoughCircles(
       men_im, mdk.mens, cv::HOUGH_GRADIENT, dp,
       minDist, canny_thresh_high, thresh_find, minRadius, maxRadius);
 
   // detect DO and KOTE
-  std::vector<std::vector<cv::Point> > contours;
-  findContours(do_kote_im, contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+  typedef std::vector<std::vector<cv::Point> > contours_t;
+  contours_t do_contours, kote_contours;
+  findContours(  do_im,   do_contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
+  findContours(kote_im, kote_contours, cv::RETR_LIST, cv::CHAIN_APPROX_SIMPLE);
 
-  for (auto&& contour : contours) {
-    // Approximate curves with lines
-    const auto arc = cv::arcLength(contour, true);
-    std::vector<cv::Point> approx;
-    cv::approxPolyDP(cv::Mat(contour), approx, 0.01*arc, true);
-    // Prune if area is too small
-    //if(cv::contourArea(approx) < 50) continue;
-    // Prune if curve is not a rectangle
-    //if(approx.size() != 4) continue;
-    // This is DO or KOTE
-    const auto mean =
-      std::accumulate(approx.begin(), approx.end(), cv::Point()) / (int)approx.size();
-    if(blue_im.at<unsigned char>(mean)) {
-      mdk.dos.push_back(approx);
-    } else {
-      mdk.kotes.push_back(approx);
+  const auto detect_do_kote = [](const auto &contours, auto &parts) {
+    for (auto&& contour : contours) {
+      // Approximate curves with lines
+      const auto arc = cv::arcLength(contour, true);
+      std::vector<cv::Point> approx;
+      cv::approxPolyDP(cv::Mat(contour), approx, 0.05*arc, true);
+      // Prune if area is too small
+      if(cv::contourArea(approx) < 30) continue;
+      // Prune if curve is not a rectangle
+      if(approx.size() > 5) continue;
+      // This is DO or KOTE
+      parts.push_back(approx);
     }
-  }
+  };
 
-  // Visualize
-  cv::cvtColor(men_im, men_visual, cv::COLOR_GRAY2BGR);
+  detect_do_kote(  do_contours, mdk.dos);
+  detect_do_kote(kote_contours, mdk.kotes);
+
+  // Draw MEN
   for (auto&& c : mdk.mens) {
     const cv::Point center(cvRound(c[0]), cvRound(c[1]));
     const int radius = cvRound(c[2]);
-
-    // Draw circle
-    //circle(men_visual, center, radius, cv::Scalar(0,0,255), 1, 8, 0);
+    circle(men_do_kote_visual, center, radius, cv::Scalar(255,255,255), 1, 8, 0);
   }
 
-  cv::cvtColor(do_kote_im, do_kote_visual, cv::COLOR_GRAY2BGR);
-  // Draw contours
-  //cv::drawContours(do_kote_visual, mdk.dos, -1, cv::Scalar(255,0,0));
-  //cv::drawContours(do_kote_visual, mdk.kotes, -1, cv::Scalar(0,255,255));
+  // Draw DO and KOTE
+  cv::drawContours(men_do_kote_visual, mdk.dos,   -1, cv::Scalar(255,255,255));
+  cv::drawContours(men_do_kote_visual, mdk.kotes, -1, cv::Scalar(255,255,255));
 
   // Re-scale
   for (auto&& c : mdk.mens) { c *= resize_scale_inv; }
@@ -160,14 +170,12 @@ int main(int argc, char * argv[]) try {
   //csensor.set_option(RS2_OPTION_ENABLE_AUTO_WHITE_BALANCE, 1);
   //csensor.set_option(RS2_OPTION_FRAMES_QUEUE_SIZE, 2);
 
-  const auto men_window = "MEN";
-  const auto do_kote_window = "DO_KOTE";
+  const auto visual_window = "MEN(red) DO(blue) KOTE(green) Visualizer";
   const auto color_window = "Color";
   const auto depth_window = "Depth";
   cv::namedWindow(color_window,   cv::WINDOW_AUTOSIZE);
   cv::namedWindow(depth_window,   cv::WINDOW_AUTOSIZE);
-  cv::namedWindow(men_window,     cv::WINDOW_AUTOSIZE);
-  cv::namedWindow(do_kote_window, cv::WINDOW_AUTOSIZE);
+  cv::namedWindow(visual_window,  cv::WINDOW_AUTOSIZE);
 
   rs2::align  align(RS2_STREAM_COLOR);
 
@@ -183,8 +191,12 @@ int main(int argc, char * argv[]) try {
     const cv::Mat depth_mat(get_cv_size(depth), CV_16UC1, (void*)depth.get_data(), cv::Mat::AUTO_STEP);
 
     // Search datotsu-parts
-    cv::Mat men_visual, do_kote_visual;
-    const auto mdk = get_men_do_kote(color_mat, men_visual, do_kote_visual);
+    cv::Mat visual;
+    const auto mdk = get_men_do_kote (
+      color_mat, depth_mat,
+      1, 1.0/depth_scale,
+      visual
+    );
 
     for (auto&& c : mdk.mens) {
       const cv::Point center(cvRound(c[0]), cvRound(c[1]));
@@ -200,8 +212,7 @@ int main(int argc, char * argv[]) try {
     // Update the window with new data
     cv::imshow(color_window, color_mat);
     cv::imshow(depth_window, depth_mat);
-    cv::imshow(men_window, men_visual);
-    cv::imshow(do_kote_window, do_kote_visual);
+    cv::imshow(visual_window, visual);
   }
 
   return EXIT_SUCCESS;
