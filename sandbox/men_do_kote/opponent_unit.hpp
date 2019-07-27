e60cec13745f68fe9ad364e7f3bccb0f56b79fe7
ikanoano <ikanoano@users.noreply.github.com>
2019-07-27 23:54:46 +0900

    add interface

---
+++ sandbox/men_do_kote/opponent_unit.hpp
@@ -0,0 +1,32 @@
+#ifndef OPPONENT_UNIT
+#define OPPONENT_UNIT
+
+#include "men_do_kote.hpp"
+
+enum class OpponentBehavior {
+  STABLE_FAR_OUT_OF_RANGE,
+  STABLE_FAR_IN_RANGE,
+  STABLE_NEAR,
+  APPROACHING_VERTICAL,
+  APPROACHING_HORIZONTAL,
+  EVACUATING
+};
+
+class OpponentUnit {
+public:
+  OpponentUnit() : m() { }
+
+  struct OpponentModel {
+    OpponentBehavior behavior;
+    float position[3][3];
+    float delta[3][3];  // Expected to be very unstable
+  };
+
+  void startCamera() {} // To be implemented
+  void stopCamera() {}  // To be implemented
+  void survey() {}      // To be implemented
+private:
+  Mikiri m;
+};
+
+#endif
