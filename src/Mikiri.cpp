e931a14b994a214b1dde4d9726af276289bbddbc
ikanoano <ikanoano@users.noreply.github.com>
2020-12-12 10:31:23 +0900

    remove dou tuning

--- src/Mikiri.cpp
+++ src/Mikiri.cpp
@@ -142,7 +142,7 @@ boost::optional<Mikiri::men_do_kote_t> Mikiri::body () {
 
   // Convert realsense coordinate system to actionplan's one
   for (auto& e : mens)  e = conv_tct(e);
-  for (auto& e : dos)  {e = conv_tct(e); e.coord[2]-=0.03;}
+  for (auto& e : dos)   e = conv_tct(e);
   for (auto& e : kotes) e = conv_tct(e);
 
   // Prune if target is too far or too near
